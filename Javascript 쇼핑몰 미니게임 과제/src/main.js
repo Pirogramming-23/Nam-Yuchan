3f080155562df2c0ecce9e13bf6ad60d4e36477b
namu627 <0627eric@naver.com>
2025-07-02 15:00:13 +0900

    JavaScript display items

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -5,10 +5,26 @@ function loadItems() {
     .then(json=>json.items);
 }
 
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    const html = items.map(item => createHTMLString(item))
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+function createHTMLString(item) {
+    return `
+    <li class="item">
+      <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+      <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
+
+//main
 loadItems()
 .then(items => {
     console.log(items);
-    // displayItems(items);
-    // setEventListeners(items);
+    displayItems(items);
+    //setEventListeners(items);
 })
 .catch(console.log);
