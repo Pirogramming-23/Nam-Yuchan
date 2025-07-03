414986497c81a8d7af9031b43e5a88697d46a473
namu627 <0627eric@naver.com>
2025-07-03 20:35:17 +0900

    9단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,3 +1,5 @@
+import random
+
 #1단계
 num=0 
 
@@ -74,7 +76,48 @@ num=0
 #         break
 #     turn = 1-turn
 
-#8단계
+# #8단계
+# def brGame(현재플레이어):
+#     global num
+#     while True:
+#         부를숫자= input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+#         try:
+#             부를숫자 = int(부를숫자)
+#             if 1 <= 부를숫자 <= 3:
+#                 break
+#             else:
+#                 print("1, 2, 3 중 하나를 입력하세요.")
+#         except ValueError:
+#             print("정수를 입력하세요.")
+
+#     for i in range(부를숫자):
+#         num += 1
+#         print(f"{현재플레이어} : {num}")
+#         if num >= 31:
+#             break
+
+# #6단계
+# turn = 0 # 0: playerA, 1: playerB
+# while True:
+#     if turn == 0:
+#          현재플레이어= "playerA"
+#     else: 
+#          현재플레이어="playerB"
+#     brGame(현재플레이어)
+
+# #7단계
+#     if num >= 31:
+#         if turn == 0:
+#             winner="playerB"
+#         else:
+#             winner="playerA"
+
+#         print(f"{winner} win!")
+#         break
+#     turn = 1-turn
+
+
+#9단계
 def brGame(현재플레이어):
     global num
     while True:
@@ -93,22 +136,27 @@ def brGame(현재플레이어):
         print(f"{현재플레이어} : {num}")
         if num >= 31:
             break
+turn = 1 # 0: player, 1: computer
 
-#6단계
-turn = 0 # 0: playerA, 1: playerB
 while True:
     if turn == 0:
-         현재플레이어= "playerA"
-    else: 
-         현재플레이어="playerB"
-    brGame(현재플레이어)
+        현재플레이어 = "player"
+        brGame(현재플레이어)
+    else:
+        현재플레이어 = "computer"
+        랜덤숫자 = random.randint(1, 3)
 
-#7단계
+        for i in range(랜덤숫자):
+            num += 1 
+            print(f"{현재플레이어} {num}")
+            if num >= 31:
+                break
+
     if num >= 31:
-        if turn == 0:
-            winner="playerB"
+        if 현재플레이어 == "computer":
+            winner="player"
         else:
-            winner="playerA"
+            winner="computer"
 
         print(f"{winner} win!")
         break
