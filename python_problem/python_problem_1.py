fc6cc0ebc23f2eed0a40ddfde7e1130273b190e5
namu627 <0627eric@naver.com>
2025-07-03 18:10:10 +0900

    5단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,10 +1,13 @@
-num=0 #1단계
+#1단계
+num=0 
 
 
-부를숫자=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ") #2단계
+#2단계
+부를숫자=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
 
 
-while True: #3단계
+#3단계
+while True: 
     try:
         부를숫자 = int(부를숫자)
         if 1 <= 부를숫자 <= 3:
@@ -17,6 +20,24 @@ while True: #3단계
         부를숫자=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
 
 
-for i in range(부를숫자): #4단계
-    num += 1 # num을 1씩 증가
+#4단계
+for i in range(부를숫자): 
+    num += 1
     print(f"playerA : {num}")
+
+
+#5단계
+while True:
+    부를숫자= input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+    try:
+        부를숫자_b = int(부를숫자)
+        if 1 <= 부를숫자_b <= 3:
+            break
+        else:
+            print("1, 2, 3 중 하나를 입력하세요")
+    except ValueError:
+        print("정수를 입력하세요.")
+
+for i in range(부를숫자_b):
+    num += 1
+    print(f"playerB : {num}")
