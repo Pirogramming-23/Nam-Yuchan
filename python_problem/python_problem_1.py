2b6e0a18293553b2ba0b7c7401f9c708d2845952
namu627 <0627eric@naver.com>
2025-07-03 20:06:16 +0900

    8단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -3,7 +3,7 @@ num=0
 
 
 # #2단계
-# 부를숫자=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+# 부를숫자= input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
 
 
 # #3단계
@@ -26,31 +26,57 @@ num=0
 #     print(f"playerA : {num}")
 
 
-#5단계
-while True:
-    부를숫자= input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-    try:
-        부를숫자 = int(부를숫자)
-        if 1 <= 부를숫자 <= 3:
-            break
-        else:
-            print("1, 2, 3 중 하나를 입력하세요")
-    except ValueError:
-        print("정수를 입력하세요.")
+# #5단계
+# while True:
+#     부를숫자= input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+#     try:
+#         부를숫자 = int(부를숫자)
+#         if 1 <= 부를숫자 <= 3:
+#             break
+#         else:
+#             print("1, 2, 3 중 하나를 입력하세요")
+#     except ValueError:
+#         print("정수를 입력하세요.")
 
-for i in range(부를숫자):
-    num += 1
-    print(f"playerB : {num}")
+# for i in range(부를숫자):
+#     num += 1
+#     print(f"playerB : {num}")
 
 
-#6단계
-turn = 0 # playerA=0, playerB=1
+# while True: 
+#     if turn == 0:
+#         현재플레이어= "playerA"
+#     else: 
+#         현재플레이어="playerB"
+#     while True:
+#         부를숫자= input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+#         try:
+#             부를숫자 = int(부를숫자)
+#             if 1 <= 부를숫자 <= 3:
+#                 break
+#             else:
+#                 print("1, 2, 3 중 하나를 입력하세요")
+#         except ValueError:
+#             print("정수를 입력하세요.")
+#     for i in range(부를숫자):
+#         num += 1
+#         print(f"{현재플레이어} : {num}")
+#         if num >= 31:
+#             break
+# #7단계        
+#     if num >= 31:
+#         if turn == 0:
+#             winner="playerB"
+#         else:
+#             winner="playerA"
 
-while True: 
-    if turn == 0:
-        현재플레이어= "playerA"
-    else: 
-        현재플레이어="playerB"
+#         print(f"{winner} win!")
+#         break
+#     turn = 1-turn
+
+#8단계
+def brGame(현재플레이어):
+    global num
     while True:
         부를숫자= input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
         try:
@@ -58,15 +84,26 @@ while True:
             if 1 <= 부를숫자 <= 3:
                 break
             else:
-                print("1, 2, 3 중 하나를 입력하세요")
+                print("1, 2, 3 중 하나를 입력하세요.")
         except ValueError:
             print("정수를 입력하세요.")
+
     for i in range(부를숫자):
         num += 1
         print(f"{현재플레이어} : {num}")
         if num >= 31:
             break
-#7단계        
+
+#6단계
+turn = 0 # 0: playerA, 1: playerB
+while True:
+    if turn == 0:
+         현재플레이어= "playerA"
+    else: 
+         현재플레이어="playerB"
+    brGame(현재플레이어)
+
+#7단계
     if num >= 31:
         if turn == 0:
             winner="playerB"
