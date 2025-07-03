bd3f6bc0bb3c90af2cec27d91d008531f02b7b8c
namu627 <0627eric@naver.com>
2025-07-03 18:28:21 +0900

    6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -2,42 +2,70 @@
 num=0 
 
 
-#2단계
-부를숫자=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+# #2단계
+# 부를숫자=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
 
 
-#3단계
-while True: 
-    try:
-        부를숫자 = int(부를숫자)
-        if 1 <= 부를숫자 <= 3:
-            break
-        else:
-            print("1, 2, 3 중 하나를 입력하세요")
-            부를숫자=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-    except ValueError:
-        print("정수를 입력하세요")
-        부를숫자=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+# #3단계
+# while True: 
+#     try:
+#         부를숫자 = int(부를숫자)
+#         if 1 <= 부를숫자 <= 3:
+#             break
+#         else:
+#             print("1, 2, 3 중 하나를 입력하세요")
+#             부를숫자=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+#     except ValueError:
+#         print("정수를 입력하세요")
+#         부를숫자=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
 
 
-#4단계
-for i in range(부를숫자): 
-    num += 1
-    print(f"playerA : {num}")
+# #4단계
+# for i in range(부를숫자): 
+#     num += 1
+#     print(f"playerA : {num}")
 
 
 #5단계
 while True:
     부를숫자= input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
     try:
-        부를숫자_b = int(부를숫자)
-        if 1 <= 부를숫자_b <= 3:
+        부를숫자 = int(부를숫자)
+        if 1 <= 부를숫자 <= 3:
             break
         else:
             print("1, 2, 3 중 하나를 입력하세요")
     except ValueError:
         print("정수를 입력하세요.")
 
-for i in range(부를숫자_b):
+for i in range(부를숫자):
     num += 1
     print(f"playerB : {num}")
+
+
+#6단계
+turn = 0 # playerA=0, playerB=1
+
+while True: 
+    if turn == 0:
+        현재플레이어= "playerA"
+    else: 
+        현재플레이어="playerB"
+    while True:
+        부를숫자= input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+        try:
+            부를숫자 = int(부를숫자)
+            if 1 <= 부를숫자 <= 3:
+                break
+            else:
+                print("1, 2, 3 중 하나를 입력하세요")
+        except ValueError:
+            print("정수를 입력하세요.")
+    for i in range(부를숫자):
+        num += 1
+        print(f"{현재플레이어} : {num}")
+        if num >= 31:
+            break
+    if num >= 31:
+        break
+    turn = 1-turn
